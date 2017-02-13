ee322acd588577cd6a71e50743d8c9f52ea4a57a
Kenneth Radunz <kenneth.radunz@gmail.com>
2017-02-13 19:24:10 +0100

    the splashScreen is done :D

--- fxclient/src/main/kotlin/ch/tkoc/fourof/fx/view/SplashScreen.kt
+++ fxclient/src/main/kotlin/ch/tkoc/fourof/fx/view/SplashScreen.kt
@@ -4,6 +4,8 @@ package ch.tkoc.fourof.fx.view
 import ch.tkoc.fx.FxView
 import ch.tkoc.fx.View
 import javafx.scene.Scene
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
 import javafx.scene.layout.Pane
 import javafx.stage.Stage
 
@@ -11,10 +13,18 @@ import javafx.stage.Stage
 class SplashScreen : View() {
 
     val root: Pane by fxml()
+    val element: ImageView by fxid()
+
+    init {
+        element.image = Image(this.javaClass.getResourceAsStream("splashScreen.png"))
+    }
 
     override fun show(primaryStage: Stage) {
-        primaryStage.isResizable = false
-        primaryStage.scene = Scene(root)
-        primaryStage.show()
+        primaryStage.apply {
+            title = "FourOf Application :D"
+            isResizable = false
+            scene = Scene(root)
+            show()
+        }
     }
 }
